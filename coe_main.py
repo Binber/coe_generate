cdbb53d9cb5cf20e063dba093a815ecc95b47c23
BruceBan <rbinbme@qq.com>
2017-12-09 11:31:38 +0800

    Add files via upload
    Update file organization

---
+++ coe_main.py
@@ -0,0 +1,7 @@
+# -*- coding = utf-8 -*-
+
+import generate_coedata as gcoe
+
+file_name = "11_HDR"
+gcoe.cdata_generate(file_name)
+
--- generate_coedata.py
+++ generate_coedata.py
@@ -3,27 +3,45 @@
 WIDTH = 30      #ROM address width
 DEPTH = 516     #ROM Depth
 
-coe_data = ""
-def cdata_strip(file_name):
+def cdata_generate(file_name):
     # read the data from the file
-    file = open(file_name+".txt", 'rb')
+    file = open(file_name+".txt", 'r')
     romdata = file.read()
     file.close()
     #data strip
-    md_data = romdata.replace('\\0D\\0A', '').replace(' ', '')
+    #print(romdata)
+    md_data = romdata.replace("\\0D\\0A","").replace(" ", "")
     #data reordering
-    global coe_data
+    coe_data = ""
     for i in range(0, DEPTH, 1):
         for j in range(0, WIDTH, 1):
             coe_data += md_data[i + j * DEPTH]
         coe_data += ('\n')
-    coe_file = open(file_name + 'coe_22hdr.txt', 'wb')
+
+    #save format coe_data
+    coe_file = open(file_name + '_coe.txt', 'w')
     coe_file.write(coe_data)
+    #release coe_data
+    coe_data = ""
     coe_file.close()
 
+    # save reversed coe_data
 
+    coe_rvsfile = open(file_name + '_coervs.txt', 'w')
+    coefile = open(file_name + "_coe.txt", 'r')
 
+    for line in coefile.readlines():
+        #remove the space from start and end
+        line_data = line.strip()
+        #reverse coe_data
+        line_rvsdata = line_data[::-1]
+        #write data
+        coe_rvsfile.write(line_rvsdata+'\n')
 
+    coe_rvsfile.close()
+
+    print ("Successfully")
 if __name__ == '__main__':
-    print 'This is generate_coedata module'
-
+    print ('This is generate_coedata module')
+else:
+    print ("Start Process")
